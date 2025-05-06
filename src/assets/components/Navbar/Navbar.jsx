1205411f0116301484cad968cb41403f11c2da38
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-06 23:26:18 +0600

    windows: github live page problem unsolved

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-red-800'>Navbar</div>
+    <div className='bg-red-800 py-10'>
+        <div className='w-[1409px] mx-auto bg-blue-600 py-10'>
+            navbar
+        </div>
+    </div>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: "/innovate-react-tailwind",
+  base: '/innovate-react-tailwind/',
 });
