5fd8f3d96de7169ad5d1cd00a43559fadc92bd0c
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-08 12:59:27 +0600

    windows: sign-in

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='bg-red-500 py-10'>
         <div className='w-[1409px] mx-auto bg-blue-300 py-10'>
-            <div className='flex'>
+            <div className='flex justify-between'>
               <div>lorem</div>
               <div>lorem</div>
               <div>lorem</div>
