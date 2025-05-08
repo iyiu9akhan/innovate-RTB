9913b1089ffb65f06a534c04907cfb946d729292
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-08 16:55:28 +0600

    windows: header prblm solved with tailwind

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,14 +3,14 @@ import logo from '../../assets/logo.png'
 
 function Navbar() {
   return (
-    <div>
+    <div className='pt-8 bg-header-bg'>
         <div className='w-[1409px] mx-auto'>
-            <div className='flex justify-between'>
+            <div className='flex justify-between items-center'>
                 <div>
                     <a href="#"><img src={logo} alt="main_logo"/></a>
                 </div>
                 <div>
-                    <ul className='flex gap-x-[50px] capitalize font-secondary text-lg font-medium text-black'>
+                    <ul className='flex gap-x-[50px] capitalize font-secondary text-lg font-medium text-white items-center'>
                         <li><a href="#">home</a></li>
                         <li><a href="#">features</a></li>
                         <li><a href="#">service</a></li>
@@ -18,9 +18,9 @@ function Navbar() {
                         <li><a href="#">blog</a></li>
                     </ul>
                 </div>
-                <div className='text-black font-secondary font-medium text-lg leading-[200%] '>
-                    <a href="#">login</a>
-                    <a href="#">register</a>
+                <div className='text-white font-secondary font-medium text-lg gap-9 flex items-center'>
+                    <button className='leading-[200%] cursor-pointer capitalize'>login</button>
+                    <button className='leading-[20px] py-5 px-12 bg-btn rounded-[6px] cursor-pointer text-black capitalize'>register</button>
                 </div>
             </div>
         </div>
