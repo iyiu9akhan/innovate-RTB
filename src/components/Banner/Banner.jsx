3344d869521863e94287fbe5c505bc5438831431
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-08 22:45:52 +0600

    windows: sign out

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import Container from '../Layout/container'
+import React from "react";
+import Container from "../Layout/container";
 
-function Banner({children}) {
+function Banner({ children }) {
   return (
     <div className="bg-[url('/src/assets/bg_shape.png')] bg-no-repeat bg-cover bg-center relative w-full pt-[92px]">
       {children}
       <Container>
-      <div className='text-center'>
-        <h1 className='font-secondary font-bold text-[56px] w-[786px] mx-auto text-white mt-[98px] mb-[20px] leading-[64px]'>Get your work done with Management Tool</h1>
-        <p className='font-secondary font-normal text-lg tracking-[-0.0025em] text-white leading-8 w-[467px] mx-auto mb-8'>The world's first project management platform that connects everything</p>
-        <input type="text" className='h-[66px] w-[480px] rounded-[6px] bg-white'/>
-      </div>
-    </Container>
+        <div className="text-center">
+          <h1 className="font-secondary font-bold text-[56px] w-[786px] mx-auto text-white mt-[98px] mb-[20px] leading-[64px]">
+            Get your work done with Management Tool
+          </h1>
+          <p className="font-secondary font-normal text-lg tracking-[-0.0025em] text-white leading-8 w-[467px] mx-auto mb-8">
+            The world's first project management platform that connects
+            everything
+          </p>
+         
+            <input
+              type="text"
+              className="h-[66px] w-[480px] rounded-[6px] bg-white mb-98px relative"
+            />
+            <button className="absolute right-0 capitalize px-[28.5px] py-[15px] bg-btn rounded-[6px] cursor-pointer">try for free</button>
+       
+        </div>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
