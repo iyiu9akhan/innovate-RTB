6ca0462c84606de06f3a47f8e0067e700e515652
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-11 22:44:15 +0600

    windows:signout

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -7,7 +7,7 @@ function Pricing() {
         <Container>
             <div>
                 <h1 className='font-primary font-bold text-[46px] leading-[56px] text-blue-color w-[623px] mb-[39px] capitalize mx-auto text-center'>pricing designed to fit your business</h1>
-                <p>monthly</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur provident, vel nisi maiores dolore laborum repellat nulla veritatis perferendis aliquam!</p>
             </div>
         </Container>
     </div>
