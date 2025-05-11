19f1a47b6ef45f95647b614ff7bf84660673fb82
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-11 15:18:00 +0600

    windows: facts section solved

--- src/components/facts/Facts.jsx
+++ src/components/facts/Facts.jsx
@@ -23,17 +23,23 @@ function Facts() {
   return (
     <div>
         <Container>
-            <div className="rounded-[40px] bg-blueBtn-color bg-[url('/assets/facts/texture.png')] bg-no-repeat flex justify-between ">
-                <div>
+            <div className="rounded-[40px] bg-blueBtn-color bg-[url('/src/assets/facts/texture.png')] bg-no-repeat flex  ">
+                <div className='mt-[44px] ml-[41px] mr-[45px] content-end'>
                     <img src={illustration} alt="#illustration" />
                 </div>
-                <div>
-                    <h4>Trusted by 35,000+ happy customers.</h4>
-                    <div className='flex justify-between'>
+                <div className='mr-[70px] mt-[85px]'>
+                    <h4 className='font-primary font-bold text-[44px] text-white mb-[64px] w-[554px]'>Trusted by 35,000+ happy customers.</h4>
+                    <div className='flex justify-between mb-[85px] w-[608px]'>
+                       {facts_data.map((item)=>(
                         <div>
-                            <h2>720+</h2>
-                            <p>Over 500 business powered with us</p>
+                            <h4 className='font-primary font-black text-[44px] leading-[54px] text-white mb-3'>
+                                {item.digit}
+                            </h4>
+                            <p className='font-secondary font-medium text-lg leading-8 text-white w-[176px]'>
+                                {item.info}
+                            </p>
                         </div>
+                       ))}
                     </div>
                 </div>
             </div>
