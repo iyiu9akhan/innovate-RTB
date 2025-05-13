fb99f7b8150a01c5988f81a0f2d250c0a411f828
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-13 14:25:11 +0600

    windows: sign in

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import Container from '../Layout/container'
+import React from "react";
+import Container from "../Layout/container";
 
 function Pricing() {
   return (
     <div>
-        <Container>
-            <div>
-                <h1 className='font-primary font-bold text-[46px] leading-[56px] text-blue-color w-[623px] mb-[39px] capitalize mx-auto text-center'>pricing designed to fit your business</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur provident, vel nisi maiores dolore laborum repellat nulla veritatis perferendis aliquam!</p>
-            </div>
-        </Container>
+      <Container>
+        <div>
+          <h1 className="font-primary font-bold text-[46px] leading-[56px] text-blue-color w-[623px] mb-[39px] capitalize mx-auto text-center">
+            pricing designed to fit your business
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
+            provident, vel nisi maiores dolore laborum repellat nulla veritatis
+            perferendis aliquam!
+          </p>
+        </div>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Pricing
+export default Pricing;
