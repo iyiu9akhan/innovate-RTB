91c9350918ec1833c9446813a564b044f2d1fb7b
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-13 16:59:42 +0600

    windows: pricing coded with raw data

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Container from "../Layout/container";
 import { FaRegUser } from "react-icons/fa";
+import { GiCheckMark } from "react-icons/gi";
+import Button from "../Layout/Button";
 
 function Pricing() {
+
+    const pricing_data =[
+        
+    ]
   return (
     <div>
       <Container>
@@ -15,12 +21,65 @@ function Pricing() {
           </h6>
         </div>
         <div className="flex justify-between ">
-          <div className="border-1 border-border-gray rounded-[20px] py-[60px] px-[40px] flex flex-col text-center items-center">
-            <div className="h-[90px] w-[90px] bg-[#E8F2FA] rounded-[50%] flex items-center justify-center mb-8">
-              <FaRegUser size={40} />
+          <div className="border-1 border-border-gray rounded-[20px] py-[60px] px-[40px] flex flex-col text-center group hover:bg-blueBtn-color transition-all duration-500">
+            <div className=" mx-auto">
+              <div className="h-[90px] w-[90px] bg-[#E8F2FA] rounded-[50%] flex items-center justify-center mb-8 group-hover:bg-white transition-all duration-500">
+                <FaRegUser size={40}  className=" text-[#2290F5] group-hover:text-blueBtn-color transition-all duration-500"/>
+              </div>
             </div>
-            <h6 className=" font-primary font-bold text-8 leading-9 capitalize text-blueBtn-color mb-[2px]">personal</h6>
-            <h2 className="font-primary font-bold text-[44px] leading-[54px] tracking-[-0.88px]">$10/mth</h2>
+            <h6 className=" font-primary font-bold text-8 leading-9 capitalize text-blueBtn-color mb-[2px] group-hover:text-btn transition-all duration-500">
+              personal
+            </h6>
+            <h2 className="font-primary font-bold text-[44px] leading-[54px] tracking-[-0.88px] mb-[14px] text-blue-color group-hover:text-white transition-all duration-500">
+              $10/mth
+            </h2>
+            <p className="font-secondary font-medium text-lg leading-[18px] mb-[40px] group-hover:text-white transition-all duration-500">
+              Annual pricing
+              <span className="text-blueBtn-color mb-[40px] transition-all duration-500 group-hover:text-btn"> (save 20%)</span>
+            </p>
+            <div className="mb-[40px]">
+              <div className="flex items-center mb-[26px]">
+                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
+                  <GiCheckMark className="text-blueBtn-color" size={12} />
+                </div>
+                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
+                  Calendar View
+                </p>
+              </div>
+              <div className="flex items-center mb-[26px]">
+                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
+                  <GiCheckMark className="text-blueBtn-color" size={12} />
+                </div>
+                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
+                  Custom Templates
+                </p>
+              </div>
+              <div className="flex items-center mb-[26px]">
+                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
+                  <GiCheckMark className="text-blueBtn-color" size={12} />
+                </div>
+                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
+                  list view
+                </p>
+              </div>
+              <div className="flex items-center mb-[26px]">
+                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
+                  <GiCheckMark className="text-blueBtn-color" size={12} />
+                </div>
+                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
+                  board view
+                </p>
+              </div>
+              <div className="flex items-center">
+                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
+                  <GiCheckMark className="text-blueBtn-color" size={12} />
+                </div>
+                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
+                  Assignees & Due Dates
+                </p>
+              </div>
+            </div>
+            <Button className="w-[330px] transition-all duration-500 group-hover:text-black group-hover:bg-btn group-hover:border-btn">Purchase Now</Button>
           </div>
         </div>
       </Container>
