ebf9d6a9b11a900fd2daea57b5ac9c06de87b46f
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-13 15:38:25 +0600

    windows: pricing section foundation

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "../Layout/container";
+import { FaRegUser } from "react-icons/fa";
 
 function Pricing() {
   return (
@@ -9,11 +10,18 @@ function Pricing() {
           <h1 className="font-primary font-bold text-[46px] leading-[56px] text-blue-color w-[623px] mb-[39px] capitalize mx-auto text-center">
             pricing designed to fit your business
           </h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
-            provident, vel nisi maiores dolore laborum repellat nulla veritatis
-            perferendis aliquam!
-          </p>
+          <h6 className="text-center mb-[77px] capitalize font-primary font-bold text-7 text-blueBtn-color">
+            monthly
+          </h6>
+        </div>
+        <div className="flex justify-between ">
+          <div className="border-1 border-border-gray rounded-[20px] py-[60px] px-[40px] flex flex-col text-center items-center">
+            <div className="h-[90px] w-[90px] bg-[#E8F2FA] rounded-[50%] flex items-center justify-center mb-8">
+              <FaRegUser size={40} />
+            </div>
+            <h6 className=" font-primary font-bold text-8 leading-9 capitalize text-blueBtn-color mb-[2px]">personal</h6>
+            <h2 className="font-primary font-bold text-[44px] leading-[54px] tracking-[-0.88px]">$10/mth</h2>
+          </div>
         </div>
       </Container>
     </div>
