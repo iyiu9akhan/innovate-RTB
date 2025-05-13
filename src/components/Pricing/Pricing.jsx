1273f99059c64356bb538382e2da62d2607d041d
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-13 19:01:23 +0600

    windows: pricing section solved

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -6,62 +6,62 @@ import Button from "../Layout/Button";
 import { HiOutlineUserGroup } from "react-icons/hi2";
 import { PiBuildingOffice } from "react-icons/pi";
 
-// import icon_one from "../../assets/pricing/price_one.png"
 
 function Pricing() {
-
-    const pricing_data =[
-        {
-            icon: <FaRegUser/>,
-            title: 'personal',
-            heading: '$10',
-            // discount: 'Annual pricing',
-            dis_digit: '20',
-            feature : [
-                {
-                    0: 'Calendar View',
-                    1: 'Custom Templates',
-                    2: 'list view',
-                    3: 'board view',
-                    4: 'Assignees & Due Dates'
-                },
-            ]
-        },
-        {
-            icon: <HiOutlineUserGroup />,
-            title: 'team',
-            heading: '$25',
-            // discount: 'Annual pricing ',
-            dis_digit: '35',
-            feature : [
-                {
-                    0: 'Timeline Review',
-                    1: 'Custom fields',
-                    2: 'Custom Templates',
-                    3: 'Task dependencies',
-                    4: 'Milestones Manage'
-                },
-            ]
-        },
-        {
-            icon: <PiBuildingOffice />,
-            title: 'enterprise',
-            heading: '$50',
-            // discount: 'Annual pricing ',
-            dis_digit: '40',
-            feature : [
-                {
-                    0: 'Data export & deletion',
-                    1: 'Block native integrations',
-                    2: 'Custom branding',
-                    3: 'Lock custom fields',
-                    4: 'Priority support'
-                },
-            ]
-        },
-    ]
+  const pricing_data = [
+    {
+      icon: <FaRegUser />,
+      icon_color: "#2290F5",
+      icon_bg: "#E8F2FA",
+      title: "personal",
+      heading: "$10",
+      dis_digit: "20",
+      feature: [
+        [
+           "Calendar View",
+           "Custom Templates",
+           "list view",
+           "board view",
+          "Assignees & Due Dates",
+     ],
+      ],
+      feature_icon: <GiCheckMark />
+    },
+    {
+      icon: <HiOutlineUserGroup />,
+      icon_color: "#3639A4",
+      icon_bg: "#EAEBF6",
+      title: "team",
+      heading: "$25",
+      dis_digit: "35",
+      feature: [
+        [ "Timeline Review",
+          "Custom fields",
+         "Custom Templates",
+          "Task dependencies",
+          "Milestones Manage",],
+      ],
+      feature_icon: <GiCheckMark />
+    },
+    {
+      icon: <PiBuildingOffice />,
+      icon_color: "#9472F9",
+      icon_bg: "#ECE8F9",
+      title: "enterprise",
+      heading: "$50",
+      dis_digit: "40",
+      feature: [
+        ["Data export & deletion",
+           "Block native integrations",
+          "Custom branding",
+          "Lock custom fields",
+           "Priority support",],
+      ],
+      feature_icon: <GiCheckMark />
+    },
+  ];
   return (
-    <div>
+    <div className="mb-[160px]">
       <Container>
         <div>
           <h1 className="font-primary font-bold text-[46px] leading-[56px] text-blue-color w-[623px] mb-[39px] capitalize mx-auto text-center">
@@ -72,70 +72,61 @@ function Pricing() {
           </h6>
         </div>
         <div className="flex justify-between ">
-        {pricing_data.map((item)=>(
-              <div className="border-1 border-border-gray rounded-[20px] py-[60px] px-[40px] flex flex-col text-center group hover:bg-blueBtn-color transition-all duration-500">
-            <div className=" mx-auto">
-              <div className="h-[90px] w-[90px] bg-[#E8F2FA] rounded-[50%] flex items-center justify-center mb-8 group-hover:bg-white transition-all duration-500">
-                <div className=" text-[#2290F5] group-hover:text-blueBtn-color transition-all duration-500 text-[40px]">
+          {pricing_data.map((item) => (
+            <div className="border-1 border-border-gray rounded-[20px] px-[60px] py-[40px] flex flex-col text-center group hover:bg-blueBtn-color transition-all duration-500">
+              <div className=" mx-auto">
+                <div
+                  style={{
+                    backgroundColor: item.icon_bg,
+                  }}
+                  className="h-[90px] w-[90px]  rounded-[50%] flex items-center justify-center mb-8 group-hover:bg-white transition-all duration-500"
+                >
+                  <div
+                    style={{
+                      color: item.icon_color,
+                    }}
+                    className="group-hover:text-blueBtn-color transition-all duration-500 text-[40px]"
+                  >
                     {item.icon}
+                  </div>
                 </div>
               </div>
+              <h6 className=" font-primary font-bold text-[24px] leading-9 capitalize text-blueBtn-color mb-[2px] group-hover:text-btn transition-all duration-500">
+                {item.title}
+              </h6>
+              <h2 className="font-primary font-bold text-[44px] leading-[54px] tracking-[-0.88px] mb-[14px] text-blue-color group-hover:text-white transition-all duration-500">
+                {item.heading}/mth
+              </h2>
+              <p className="font-secondary font-medium text-lg leading-[18px] mb-[40px] group-hover:text-white transition-all duration-500">
+                Annual pricing
+                <span className="text-blueBtn-color mb-[40px] transition-all duration-500 group-hover:text-btn">
+                  {" "}
+                  (save {item.dis_digit}%)
+                </span>
+              </p>
+              <div className="mb-[40px]">
+                {item.feature[0].map((list, index) => (
+                  <div
+                    className="flex items-center mb-[26px]"
+                    key={index}
+                  >
+                    <div className="flex items-center">
+                      <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center text-blueBtn-color text-[12px]">
+                        {item.feature_icon}
+                      </div>
+                      <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white ">
+                        {" "}
+                        {list}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <Button className="w-[330px] transition-all duration-500 group-hover:text-black group-hover:bg-btn group-hover:border-btn hover:bg-white hover:border-white">
+                Purchase Now
+              </Button>
             </div>
-            <h6 className=" font-primary font-bold text-[24px] leading-9 capitalize text-blueBtn-color mb-[2px] group-hover:text-btn transition-all duration-500">
-              {item.title}
-            </h6>
-            <h2 className="font-primary font-bold text-[44px] leading-[54px] tracking-[-0.88px] mb-[14px] text-blue-color group-hover:text-white transition-all duration-500">
-              {item.heading}/mth
-            </h2>
-            <p className="font-secondary font-medium text-lg leading-[18px] mb-[40px] group-hover:text-white transition-all duration-500">
-              Annual pricing
-              <span className="text-blueBtn-color mb-[40px] transition-all duration-500 group-hover:text-btn"> (save {item.dis_digit}%)</span>
-            </p>
-            <div className="mb-[40px]">
-              <div className="flex items-center mb-[26px]">
-                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
-                  <GiCheckMark className="text-blueBtn-color" size={12} />
-                </div>
-                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
-                  Calendar View
-                </p>
-              </div>
-              <div className="flex items-center mb-[26px]">
-                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
-                  <GiCheckMark className="text-blueBtn-color" size={12} />
-                </div>
-                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
-                  Custom Templates
-                </p>
-              </div>
-              <div className="flex items-center mb-[26px]">
-                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
-                  <GiCheckMark className="text-blueBtn-color" size={12} />
-                </div>
-                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
-                  list view
-                </p>
-              </div>
-              <div className="flex items-center mb-[26px]">
-                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
-                  <GiCheckMark className="text-blueBtn-color" size={12} />
-                </div>
-                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
-                  board view
-                </p>
-              </div>
-              <div className="flex items-center">
-                <div className="h-[24px] w-[24px] bg-[#F4EBFF] rounded-[50%] flex justify-center items-center">
-                  <GiCheckMark className="text-blueBtn-color" size={12} />
-                </div>
-                <p className="ml-[22px] font-secondary font-medium text-lg leading-[32px] text-info-color transition-all duration-500 group-hover:text-white">
-                  Assignees & Due Dates
-                </p>
-              </div>
-            </div>
-            <Button className="w-[330px] transition-all duration-500 group-hover:text-black group-hover:bg-btn group-hover:border-btn">Purchase Now</Button>
-          </div>
-        ))}
+          ))}
         </div>
       </Container>
     </div>
