7c79ecd58f766388d6509c6fd2d973405267daf3
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-15 19:56:41 +0600

    windows: contact section foudation

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Container from '../Layout/container'
+import Button from "../Layout/Button";
+
 
 const Contact = () => {
   return (
     <div>
         <Container>
-            {/* <div>Lorem ipsum dolor sit amet.</div> */}
+            <div className='mt-[160px] bg-blueBtn-color rounded-[20px] px-[102px] py-[100px] flex justify-between items-center'>
+              <h1 className='font-primary font-bold text-[46px] leading-[58px] text-white w-[577px]'>Don’t find the answer? contact us for any query.</h1>
+              <Button className="bg-btn !text-black border-btn !border-2 hover:!border-btn hover:!text-white">contact us</Button>
+            </div>
         </Container>
     </div>
   )
