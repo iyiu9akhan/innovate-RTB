de48e1cb2de7e75d84de3e9fe952e6179a7bbd15
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-22 19:26:45 +0600

    windows: responsive - services section hover problem solved

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -51,15 +51,15 @@ function Services() {
         </div>
         <div className="flex flex-col items-center md:flex md:flex-row md:justify-between mb-20">
           {services_data.map((item) => (
-            <div className=" table border-1 border-[#DCE7FE] rounded-[20px] p-9 hover:bg-blueBtn-color group transition-all duration-500 mb-[30px] md:mb-0 ">
+            <div className=" table border-1 border-[#DCE7FE] rounded-[20px] p-9 bg-blueBtn-color md:bg-transparent md:hover:bg-blueBtn-color group md:transition-all md:duration-500 mb-[30px] md:mb-0 ">
               <img src={item.img} alt="#features_one" className="mb-[26px]" />
-              <h5 className="mb-2 font-primary font-bold text-[22px] leading-6 text-blue-color group-hover:text-white transition-all duration-500">
+              <h5 className="mb-2 font-primary font-bold text-[22px] leading-6 text-white md:text-blue-color md:group-hover:text-white md:transition-all md:duration-500">
                 {item.title}
               </h5>
-              <p className="font-secondary text-4 leading-[26px] tracking-[-0.04px] w-[258px] mb-[26px] text-info-color group-hover:text-white transition-all duration-500">
+              <p className="font-secondary text-4 leading-[26px] tracking-[-0.04px] w-[258px] mb-[26px]  text-white md:text-info-color md:group-hover:text-white md:transition-all md:duration-500">
                 {item.details}
               </p>
-              <button className="cursor-pointer text-[#3639A4] font-secondary text-4 font-medium leading-[26px] group-hover:text-white transition-all duration-500 flex gap-2 items-center">
+              <button className="cursor-pointer  font-secondary text-4 font-medium leading-[26px] text-white md:text-[#3639A4] md:group-hover:text-white md:transition-all md:duration-500 flex gap-2 items-center">
                 {item.button}
                 <IoMdArrowRoundForward />
               </button>
