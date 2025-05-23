f158c88650b66e0db116d5442b18816ba7729d01
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-23 23:02:45 +0600

    windows: responsive - flowbite slider issue

--- src/components/Layout/Slider.jsx
+++ src/components/Layout/Slider.jsx
@@ -10,8 +10,16 @@ function Slider() {
       >
         <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
           <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            {/* <img src="/docs/images/carousel/carousel-1.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."> */}
-            <h1>Lorem ipsum dolor sit amet.</h1>
+            {items.map((item, index) => (
+              <div
+                key={index}
+                className={`transition-opacity duration-700 ease-in-out ${
+                  index === currentIndex ? "opacity-100" : "opacity-0 absolute"
+                }`}
+              >
+                {item}
+              </div>
+            ))}
           </div>
           <div className="hidden duration-700 ease-in-out" data-carousel-item>
             {/* <img src="/docs/images/carousel/carousel-2.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."> */}
@@ -72,7 +80,7 @@ function Slider() {
           className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
           data-carousel-prev
         >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none ">
             <svg
               className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180 bg-blueBtn-color"
               aria-hidden="true"
