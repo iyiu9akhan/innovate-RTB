deb916341109339f33b998fcc18d64df02611d96
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-27 14:59:29 +0600

    windows: final formating

--- src/components/Layout/Button.jsx
+++ src/components/Layout/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({children, className}) {
   return (
     <div>
-      <button className= {`border-2 border-blueBtn-color capitalize font-secondary font-medium text-lg leading-[20px] text-white rounded-[6px] w-[196px] h-[60px]  bg-blueBtn-color cursor-pointer hover:bg-transparent hover:text-black hover:border-2 hover:border-solid hover:border-blueBtn-color transition-all duration-300 ${className}`}>
+      <button className= {`w-[130px] h-[40px] text-[14px] border-2 border-blueBtn-color capitalize font-secondary font-medium md:text-lg leading-[20px] text-white rounded-[6px] md:w-[196px] md:h-[60px]  bg-blueBtn-color cursor-pointer hover:bg-transparent hover:text-black hover:border-2 hover:border-solid hover:border-blueBtn-color transition-all duration-300 ${className}`}>
        {children}
       </button>
     </div>
